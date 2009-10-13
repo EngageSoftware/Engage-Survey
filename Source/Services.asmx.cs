93c46c00020912c591d6c49c7b36234cec282fc8
Brian Dukes <bdukes@engagesoftware.com>
2009-10-13 21:26:32 +0000

    Cleaned up Services
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Survey;C4776

--- Source/Services.asmx.cs
+++ Source/Services.asmx.cs
@@ -1,27 +1,36 @@
-﻿using System.Web.Services;
+﻿// <copyright file="Services.asmx.cs" company="Engage Software">
+// Engage: Survey
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.Survey
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Script.Services;
+    using System.Web.Services;
     using Engage.Survey.Entities;
 
     /// <summary>
-    /// Summary description for Services
+    /// Web Services for Engage: Survey
     /// </summary>
     [WebService(Namespace = "http://services.engagesoftware.com/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     [ScriptService]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
     public class Services : WebService
     {
         /// <summary>
         /// Gets a completed survey.
         /// </summary>
         /// <param name="responseHeaderId">The response header id.</param>
-        /// <returns></returns>
+        /// <returns>A <see cref="ReadonlySurvey"/> instance</returns>
         [WebMethod]
         public ReadonlySurvey GetCompletedSurvey(int responseHeaderId)
         {
@@ -32,7 +41,7 @@ namespace Engage.Dnn.Survey
         /// Gets the survey.
         /// </summary>
         /// <param name="surveyId">The survey id.</param>
-        /// <returns></returns>
+        /// <returns>A <see cref="Survey"/> instance</returns>
         [WebMethod]
         public Survey GetSurvey(int surveyId)
         {
@@ -40,14 +49,13 @@ namespace Engage.Dnn.Survey
         }
         
         /// <summary>
-        /// Gets the surveys.
+        /// Gets all surveys.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A <see cref="List{Survey}"/> of all <see cref="Survey"/>s</returns>
         [WebMethod]
         public List<Survey> GetSurveys()
         {
-            List<Survey> surveys = new List<Survey>(Survey.LoadSurveys());
-            return surveys;
+            return Survey.LoadSurveys().ToList();
         }
     }
 }

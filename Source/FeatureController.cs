c101964589a2624c2a6bf07b50a3f47901801465
Brian Dukes <bdukes@engagesoftware.com>
2009-10-13 15:27:46 +0000

    Added DNN 5 manifest and updated build script to take advantage of it
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Survey;C4672

---
+++ Source/FeatureController.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="FeatureController.cs" company="Engage Software">
+// Engage: Survey
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+namespace Engage.Dnn.Survey
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Registered with DNN to indicate which features this module supports
+    /// </summary>
+    public class FeatureController
+    {
+    }
+}

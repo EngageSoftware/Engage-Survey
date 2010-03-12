f74d7fb8c1b4004a64a6d7a55224364e306f5ffd
Brian Dukes <bdukes@engagesoftware.com>
2010-03-12 18:15:19 +0000

    Fixed issue where install threw an exception on DNN 5.
    git-tfs-work-item:1974 associate
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Survey;C8201

--- Source/FeatureController.cs
+++ Source/FeatureController.cs
@@ -11,8 +11,11 @@
 
 namespace Engage.Dnn.Survey
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
+    using System.Reflection;
     using DotNetNuke.Common.Utilities;
     using DotNetNuke.Entities.Modules;
     using DotNetNuke.Services.Packages;
@@ -27,32 +30,98 @@ namespace Engage.Dnn.Survey
         /// </summary>
         /// <param name="version">The version number.</param>
         /// <returns>A status message</returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId = "0#", Justification = "Interface was defined in VB, with incorrect naming convention")]
+        [SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId = "0#", Justification = "Interface was defined in VB, with incorrect naming convention")]
         public string UpgradeModule(string version)
         {
             switch (version)
             {
                 case "3.0.0":
-                    var configDocument = Config.Load();
-
-                    using (Stream xmlMergeManifest = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Engage.Dnn.Survey.Util.Net35.withoutNamespaces.config"))
+                    if (GetDotNetNukeFrameworkVersion().Major > 4)
                     {
-                        var configMerge = new XmlMerge(xmlMergeManifest, "3.0.0", "Engage: Survey");
-                        configMerge.UpdateConfig(configDocument);
+                        // They moved XmlMerge in DNN 5, so this only works for DNN 4
+                        break;
                     }
 
-                    using (Stream bindingRedirectManifest = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("Engage.Dnn.Survey.Util.Net35.bindingRedirect.config"))
+                    var frameworkVersion = GetNETFrameworkVersion();
+                    if (frameworkVersion.Major >= 3 && frameworkVersion.MajorRevision >= 5)
                     {
-                        var bindingRedirectMerge = new XmlMergeWithNamespaceSupport(bindingRedirectManifest);
-                        bindingRedirectMerge.UpdateConfig(configDocument);
+                        break;
                     }
 
-                    Config.Save(configDocument);
+                    UpdateWebConfigFrameworkVersion();
 
                     return "Updated web.config to support .NET 3.5";
                 default:
-                    return "Module upgraded to version " + version;
+                    break;
             }
+
+            return "Module upgraded to version " + version;
+        }
+
+        /// <summary>
+        /// Gets the <c>DotNetNuke</c> framework version.
+        /// </summary>
+        /// <remarks>Based on http://www.ifinity.com.au/Blog/EntryId/75/Include-jQuery-in-a-DotNetNuke-Module-with-version-independent-code</remarks>
+        /// <returns>The version of <c>DotNetNuke</c> that this module is running under</returns>
+        private static Version GetDotNetNukeFrameworkVersion()
+        {
+            return Assembly.GetAssembly(typeof(ModuleInfo)).GetName().Version;
+        }
+
+        /// <summary>
+        /// Gets the .NET framework version.
+        /// </summary>
+        /// <remarks>Based on <c>DotNetNuke.Common.Initialize.GetNETFrameworkVersion</c> from DNN 5.2.2</remarks>
+        /// <returns>The version of .NET that this application is running under</returns>
+        private static Version GetNETFrameworkVersion()
+        {
+// ReSharper disable EmptyGeneralCatchClause
+            var version = Environment.Version.ToString(2);
+
+            // Try and load a 3.0 Assembly 
+            try
+            {
+                AppDomain.CurrentDomain.Load("System.Runtime.Serialization, Version=3.0.0.0, Culture=neutral, PublicKeyToken=B77A5C561934E089");
+                version = "3.0";
+            }
+            catch
+            {
+            }
+
+            // Try and load a 3.5 Assembly
+            try
+            {
+                AppDomain.CurrentDomain.Load("System.Core, Version=3.5.0.0, Culture=neutral, PublicKeyToken=B77A5C561934E089");
+                version = "3.5";
+            }
+            catch
+            {
+            }
+
+// ReSharper restore EmptyGeneralCatchClause
+            return new Version(version);
+        }
+
+        /// <summary>
+        /// Updates the web.config to support .NET 3.5.
+        /// </summary>
+        private static void UpdateWebConfigFrameworkVersion()
+        {
+            var configDocument = Config.Load();
+
+            using (Stream xmlMergeManifest = Assembly.GetExecutingAssembly().GetManifestResourceStream("Engage.Dnn.Survey.Util.Net35.withoutNamespaces.config"))
+            {
+                var configMerge = new XmlMerge(xmlMergeManifest, "3.0.0", "Engage: Survey");
+                configMerge.UpdateConfig(configDocument);
+            }
+
+            using (Stream bindingRedirectManifest = Assembly.GetExecutingAssembly().GetManifestResourceStream("Engage.Dnn.Survey.Util.Net35.bindingRedirect.config"))
+            {
+                var bindingRedirectMerge = new XmlMergeWithNamespaceSupport(bindingRedirectManifest);
+                bindingRedirectMerge.UpdateConfig(configDocument);
+            }
+
+            Config.Save(configDocument);
         }
     }
 }

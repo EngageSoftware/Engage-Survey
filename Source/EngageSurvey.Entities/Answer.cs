38bdf21bc330e98756bec591fb1d8369b67fabf4
Brian Dukes <bdukes@engagesoftware.com>
2010-03-26 16:00:45 +0000

    Formatted Answer.cs
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Survey;C8386

--- Source/EngageSurvey.Entities/Answer.cs
+++ Source/EngageSurvey.Entities/Answer.cs
@@ -11,10 +11,13 @@
 
 namespace Engage.Survey.Entities
 {
-    using System;
     using System.Collections.Generic;
+    using UI;
     using Util;
 
+    /// <summary>
+    /// An answer to a <see cref="Question"/>
+    /// </summary>
     public partial class Answer : IAnswer
     {
         /// <summary>
@@ -22,100 +25,114 @@ namespace Engage.Survey.Entities
         /// </summary>
         /// <param name="revisingUser">The revising user.</param>
         public Answer(int revisingUser)
-            : this()
+                : this()
         {
             this.CreatedBy = this.RevisingUser = revisingUser;
         }
 
         /// <summary>
-        /// Returns the formatting for the element plus the the unformatted text together. Used primarily by
+        /// Gets the formatting for the element plus the the unformatted text together. Used primarily by
         /// the Web and Windows viewers only.
         /// </summary>
-        /// <value></value>
         public string FormattedText
         {
-            get { return this.Formatting + this.UnformattedText; }
+            get
+            {
+                return this.Formatting + this.UnformattedText;
+            }
         }
 
         /// <summary>
-        /// Returns only the text value of Text attribute for the survey element.
+        /// Gets the formatting that will be used to prefix the unformatted text for the survey element.
         /// </summary>
-        /// <value></value>
-        public string UnformattedText
-        {
-            get { return this.Text; }
-        }
-
-        /// <summary>
-        /// Returns the formatting that will be used to prefix the unformatted text for the survey element.
-        /// </summary>
-        /// <value></value>
         public string Formatting
         {
             get
             {
-                //none
+                // none
                 return string.Empty;
             }
         }
 
         /// <summary>
-        /// Gets the rendering key used by the SurveyControl to uniquely identify this element.
+        /// Gets the rendering key used by the <see cref="SurveyControl"/> to uniquely identify this element.
         /// </summary>
         /// <value>The rendering key.</value>
         public Key RelationshipKey
         {
             get
             {
-                return new Key{ SectionId = this.Question.SectionId, QuestionId = this.QuestionId, AnswerId = this.AnswerId};
+                return new Key { SectionId = this.Question.SectionId, QuestionId = this.QuestionId, AnswerId = this.AnswerId };
             }
         }
 
         /// <summary>
-        /// RelativeOrderComparer
+        /// Gets only the text value of Text attribute for the survey element.
+        /// </summary>
+        /// <value></value>
+        public string UnformattedText
+        {
+            get
+            {
+                return this.Text;
+            }
+        }
+
+        /// <summary>
+        /// Compares answers based on their <see cref="Answer.RelativeOrder"/>
         /// </summary>
         internal class RelativeOrderComparer : IComparer<IAnswer>
         {
+            /// <summary>
+            /// Whether the sorting is descending or ascending
+            /// </summary>
             private readonly bool descending = true;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="RelativeOrderComparer"/> class.
+            /// </summary>
             public RelativeOrderComparer()
             {
             }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="RelativeOrderComparer"/> class.
+            /// </summary>
+            /// <param name="descending">if set to <c>true</c> sorts by descending relative order; otherwise, ascending.</param>
             public RelativeOrderComparer(bool descending)
             {
                 this.descending = descending;
             }
 
-            #region IComparer Members
-
             /// <summary>
             /// Compares two objects and returns a value indicating whether one is less than, equal to, or greater than the other.
             /// </summary>
+            /// <param name="x">The first object to compare.</param>
+            /// <param name="y">The second object to compare.</param>
             /// <returns>
-            /// Value 
-            ///                     Condition 
-            ///                     Less than zero
-            ///                 <paramref name="x"/> is less than <paramref name="y"/>.
-            ///                     Zero
-            ///                 <paramref name="x"/> equals <paramref name="y"/>.
-            ///                     Greater than zero
-            ///                 <paramref name="x"/> is greater than <paramref name="y"/>.
+            /// Value
+            /// Condition
+            /// Less than zero
+            /// <paramref name="x"/> is less than <paramref name="y"/>.
+            /// Zero
+            /// <paramref name="x"/> equals <paramref name="y"/>.
+            /// Greater than zero
+            /// <paramref name="x"/> is greater than <paramref name="y"/>.
             /// </returns>
-            /// <param name="x">The first object to compare.
-            ///                 </param><param name="y">The second object to compare.
-            ///                 </param>
             public int Compare(IAnswer x, IAnswer y)
             {
-                if (x == null || y == null) return 0;
+                if (x == null || y == null)
+                {
+                    return 0;
+                }
 
                 if (this.descending)
+                {
                     return x.RelativeOrder.CompareTo(y.RelativeOrder);
+                }
+
                 return y.RelativeOrder.CompareTo(x.RelativeOrder);
             }
-
-            #endregion
         }
-
     }
 }

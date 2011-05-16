bb5cbfcd845db4638f580c10263e99692f525fc4
Brian Dukes <bdukes@engagesoftware.com>
2011-05-16 22:00:50 +0000

    Fixed incompatibility with jQuery 1.6.1 using attr('checked') (instead of the new prop('checked') or is(':checked'))
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Survey;C14899

--- Source/JavaScript/survey.js
+++ Source/JavaScript/survey.js
@@ -1,4 +1,7 @@
 /// <reference path="jquery-1.3.2.debug-vsdoc.js" />
+
+/*ignore jslint start*/
+
 /* json2 */
 if (!this.JSON) { JSON = {} } (function() { function f(n) { return n < 10 ? "0" + n : n } if (typeof Date.prototype.toJSON !== "function") { Date.prototype.toJSON = function(key) { return this.getUTCFullYear() + "-" + f(this.getUTCMonth() + 1) + "-" + f(this.getUTCDate()) + "T" + f(this.getUTCHours()) + ":" + f(this.getUTCMinutes()) + ":" + f(this.getUTCSeconds()) + "Z" }; String.prototype.toJSON = Number.prototype.toJSON = Boolean.prototype.toJSON = function(key) { return this.valueOf() } } var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, gap, indent, meta = { "\b": "\\b", "\t": "\\t", "\n": "\\n", "\f": "\\f", "\r": "\\r", '"': '\\"', "\\": "\\\\" }, rep; function quote(string) { escapable.lastIndex = 0; return escapable.test(string) ? '"' + string.replace(escapable, function(a) { var c = meta[a]; return typeof c === "string" ? c : "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4) }) + '"' : '"' + string + '"' } function str(key, holder) { var i, k, v, length, mind = gap, partial, value = holder[key]; if (value && typeof value === "object" && typeof value.toJSON === "function") { value = value.toJSON(key) } if (typeof rep === "function") { value = rep.call(holder, key, value) } switch (typeof value) { case "string": return quote(value); case "number": return isFinite(value) ? String(value) : "null"; case "boolean": case "null": return String(value); case "object": if (!value) { return "null" } gap += indent; partial = []; if (Object.prototype.toString.apply(value) === "[object Array]") { length = value.length; for (i = 0; i < length; i += 1) { partial[i] = str(i, value) || "null" } v = partial.length === 0 ? "[]" : gap ? "[\n" + gap + partial.join(",\n" + gap) + "\n" + mind + "]" : "[" + partial.join(",") + "]"; gap = mind; return v } if (rep && typeof rep === "object") { length = rep.length; for (i = 0; i < length; i += 1) { k = rep[i]; if (typeof k === "string") { v = str(k, value); if (v) { partial.push(quote(k) + (gap ? ": " : ":") + v) } } } } else { for (k in value) { if (Object.hasOwnProperty.call(value, k)) { v = str(k, value); if (v) { partial.push(quote(k) + (gap ? ": " : ":") + v) } } } } v = partial.length === 0 ? "{}" : gap ? "{\n" + gap + partial.join(",\n" + gap) + "\n" + mind + "}" : "{" + partial.join(",") + "}"; gap = mind; return v } } if (typeof JSON.stringify !== "function") { JSON.stringify = function(value, replacer, space) { var i; gap = ""; indent = ""; if (typeof space === "number") { for (i = 0; i < space; i += 1) { indent += " " } } else { if (typeof space === "string") { indent = space } } rep = replacer; if (replacer && typeof replacer !== "function" && (typeof replacer !== "object" || typeof replacer.length !== "number")) { throw new Error("JSON.stringify") } return str("", { "": value }) } } if (typeof JSON.parse !== "function") { JSON.parse = function(text, reviver) { var j; function walk(holder, key) { var k, v, value = holder[key]; if (value && typeof value === "object") { for (k in value) { if (Object.hasOwnProperty.call(value, k)) { v = walk(value, k); if (v !== undefined) { value[k] = v } else { delete value[k] } } } } return reviver.call(holder, key, value) } cx.lastIndex = 0; if (cx.test(text)) { text = text.replace(cx, function(a) { return "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4) }) } if (/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, "@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) { j = eval("(" + text + ")"); return typeof reviver === "function" ? walk({ "": j }, "") : j } throw new SyntaxError("JSON.parse") } } })();
 /*!
@@ -61,6 +64,9 @@ if (!Array.prototype.indexOf) {
 /*     Engage: Survey     */
 /*                        */
 /**************************/
+
+/*ignore jslint end*/
+
 ;(function($){
     $(function () {
         $.validator.addMethod("email", function (value, element) {
@@ -74,15 +80,15 @@ if (!Array.prototype.indexOf) {
                 DefineAnswerType: { min: 1 },
                 required: { required: true },
                 notificationEmail: { 
-                    required: { depends: function (element) { return $('#EvalSendNotification').attr('checked'); } },
+                    required: { depends: function (element) { return $('#EvalSendNotification').is(':checked'); } },
                     email: true
                 },
                 notificationEmails: { 
-                    required: { depends: function (element) { return $('#EvalSendNotification').attr('checked'); } }, 
+                    required: { depends: function (element) { return $('#EvalSendNotification').is(':checked'); } }, 
                     emails: true
                 },
                 thankYouEmail: { 
-                    required: { depends: function (element) { return $('#EvalSendThankYou').attr('checked'); } }, 
+                    required: { depends: function (element) { return $('#EvalSendThankYou').is(':checked'); } }, 
                     email: true
                 }
             },
@@ -195,11 +201,12 @@ if (!Array.prototype.indexOf) {
                 data: JSON.stringify(parameters),
                 contentType: "application/json; charset=utf-8",
                 dataFilter: function (data) {
-                    var msg = eval('(' + data + ')');
-                    if (msg.hasOwnProperty('d'))
+                    var msg = typeof(data) === 'string' ? JSON.parse(data) : data;
+                    if (msg.hasOwnProperty('d')) {
                         return msg.d;
-                    else
+                    } else { 
                         return msg;
+                    }
                 },
                 success: function (msg) { 
                     if ($.isFunction(callback)) {
@@ -231,10 +238,10 @@ if (!Array.prototype.indexOf) {
 					PreStartMessage: $('#EvalPreStartTextArea').val(),
 					EndDate: endDatePicker.get_selectedDate(),
 					PostEndMessage: $('#EvalPostEndTextArea').val(),
-					SendNotification: $('#EvalSendNotification').attr('checked'),
+					SendNotification: $('#EvalSendNotification').is(':checked'),
 					NotificationFromEmailAddress: $('#EvalNotificationFromEmail').val(),
 					NotificationToEmailAddresses: $('#EvalNotificationToEmails').val(),
-					SendThankYou: $('#EvalSendThankYou').attr('checked'),
+					SendThankYou: $('#EvalSendThankYou').is(':checked'),
 					ThankYouFromEmailAddress: $('#EvalThankYouFromEmail').val(),
                     PortalId: CurrentContextInfo.PortalId,
                     ModuleId: CurrentContextInfo.ModuleId,
@@ -257,10 +264,10 @@ if (!Array.prototype.indexOf) {
             storePreviousValue($('#EvalPreStartTextArea'));
             storePreviousValue($('.ee-end-date .RadPicker'), endDatePicker.get_selectedDate());
             storePreviousValue($('#EvalPostEndTextArea'));
-            storePreviousValue($('#EvalSendNotification'), $('#EvalSendNotification').attr('checked'));
+            storePreviousValue($('#EvalSendNotification'), $('#EvalSendNotification').is(':checked'));
             storePreviousValue($('#EvalNotificationFromEmail'));
             storePreviousValue($('#EvalNotificationToEmails'));
-            storePreviousValue($('#EvalSendThankYou'), $('#EvalSendThankYou').attr('checked'));
+            storePreviousValue($('#EvalSendThankYou'), $('#EvalSendThankYou').is(':checked'));
             storePreviousValue($('#EvalThankYouFromEmail'));
             
             makeLabelEditable($('#EvalTitleInput'), $('<input type="text"/>'));
@@ -351,10 +358,10 @@ if (!Array.prototype.indexOf) {
                 $('.ee-timeframe').slideUp(AnimationSpeed);
             }
             
-            makeOptionalElementReadonly($('#EvalSendNotification'), $('.ee-notification'), $('#EvalSendNotification').attr('checked') ? CurrentContextInfo.CheckBoxCheckedText : CurrentContextInfo.CheckBoxUncheckedText);
+            makeOptionalElementReadonly($('#EvalSendNotification'), $('.ee-notification'), $('#EvalSendNotification').is(':checked') ? CurrentContextInfo.CheckBoxCheckedText : CurrentContextInfo.CheckBoxUncheckedText);
             makeOptionalElementReadonly($('#EvalNotificationFromEmail'), $('.ee-notification-from'));
             makeOptionalElementReadonly($('#EvalNotificationToEmails'), $('.ee-notification-to'));
-            makeOptionalElementReadonly($('#EvalSendThankYou'), $('.ee-thankyou'), $('#EvalSendThankYou').attr('checked') ? CurrentContextInfo.CheckBoxCheckedText : CurrentContextInfo.CheckBoxUncheckedText);
+            makeOptionalElementReadonly($('#EvalSendThankYou'), $('.ee-thankyou'), $('#EvalSendThankYou').is(':checked') ? CurrentContextInfo.CheckBoxCheckedText : CurrentContextInfo.CheckBoxUncheckedText);
             makeOptionalElementReadonly($('#EvalThankYouFromEmail'), $('.ee-thankyou-from'));
             
             $('.ee-email.ee-expanded legend a').click();
@@ -399,7 +406,7 @@ if (!Array.prototype.indexOf) {
                         .data('maxlength', maxlength || '')
                         .data('rows', $this.attr('rows') || '')
                         .data('cols', $this.attr('cols') || '')
-                        .data('checked', $this.attr('checked') !== undefined ? $this.attr('checked') : '')
+                        .data('checked', $this.attr('checked') !== undefined ? $this.is(':checked') : '')
                         .addClass('ee-input-pre')
                         .text(value || $this.val())
                         .hide()
